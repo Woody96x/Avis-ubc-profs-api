674f13da2a0c7230b7ff950c5a3f79318ffd2d22
Christopher Hui <christophergkhui@gmail.com>
2023-01-08 19:51:40 -0500

    added new years to db

--- api.py
+++ api.py
@@ -1,12 +1,10 @@
 import flask
 from flask_restful import Api
 from flask_cors import CORS
-from flask_marshmallow import Marshmallow
 import os
 
 app = flask.Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-ma = Marshmallow(app)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['DEBUG'] = True
